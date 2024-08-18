1df7b54c0b44dd9ecf3896661de20ad2ed233bc6
deepali04 <nagwade.deepali@gmail.com>
2024-08-17 23:44:09 -0400

    Updated education

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -4,50 +4,49 @@ import { makeStyles } from "@material-ui/core/styles";
 import { TextDecrypt } from "../content/TextDecrypt";
 import './Education.css';
 
-const useStyles = makeStyles((theme) => ({
-  main: {
-    maxWidth: '100vw',
-    marginTop: '3em',
-    marginBottom: "3em",
-  },
-}));
+const EducationCard = ({ institution, studyType, course, duration, gpa, courses }) => (
+  <div className="experience-card">
+    <h3 className="experience-title">{institution}</h3>
+    <h4 className="experience-subtitle">{`${studyType}, ${course} | GPA: ${gpa}`}</h4>
+    <p className="experience-duration">{duration}</p>
+    <ul className="experience-details">
+      {courses.map((detail, index) => (
+        <li key={index}>{detail}</li>
+      ))}
+    </ul>
+  </div>
+);
 
 export const Education = () => {
-  const classes = useStyles();
-  const greetings = "My education.";
 
   const educations = [
     {
-      school: "University 1",
-      degree: "Degree 1",
-      duration: "Duration 1",
-      description: "Description 1",
-    },
-    {
-      school: "University 2",
-      degree: "Degree 2",
-      duration: "Duration 2",
-      description: "Description 2",
-    },
+      "institution": "Stevens Institute of Technology, New Jersey",
+      "course": "Computer Science",
+      "studyType": "MS",
+      "duration": "Sept 22 - May 24",
+      "gpa": "3.8",
+      "courses": ["Web Programming", "Software Design and Analysis", "Human Computer Interaction", "Data Mining"]
+  },
+  {
+    "institution": "Shri GS Institute of Technology and Science, India",
+    "course": "Computer Science",
+    "studyType": "BS",
+    "duration": "Aug 2014 - May 2018",
+    "gpa": "3.2",
+    "courses": ["Distributed Systems", "Data Structures", "Systems programming", "Object-oriented Design"]
+}
     // Add more education entries as needed
   ];
 
   return (
-    <section id="education">
-      <Container component="main" className={classes.main} maxWidth="md">
-        <div className="education-container">
-          {educations.map((edu, index) => (
-            <div key={index} className="education">
-              <div className="_form_wrapper">
-                <h3>{edu.school}</h3>
-                <h4>{edu.degree}</h4>
-                <p>{edu.duration}</p>
-                <p>{edu.description}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </Container>
+    <section className="experience-section">
+      <h2 className="section-title">Education</h2>
+      <div className="experience-container">
+        {educations.map((edu, index) => (
+          <EducationCard key={index} {...edu} />
+        ))}
+      </div>
     </section>
   );
 };
