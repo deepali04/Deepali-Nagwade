b4486faff07baa0c81674c0d4da85106178467a7
deepali04 <nagwade.deepali@gmail.com>
2024-08-27 20:51:17 -0400

    updated themeprovider

--- src/components/theme/ThemeProvider.js
+++ src/components/theme/ThemeProvider.js
@@ -5,20 +5,16 @@ import { MuiThemeProvider } from "@material-ui/core/styles";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const getInitialMode = () => {
-        if (typeof localStorage === "undefined") return "dark"; // Default to dark mode if no localStorage
-        const savedMode = localStorage.getItem("theme");
-        return savedMode ? savedMode : "dark"; // Use saved mode or default to dark
-    };
-
-    const [theme, setTheme] = useState(getInitialMode());
+    // Start with dark mode as the default
+    const [theme, setTheme] = useState("dark");
 
     const toggleTheme = () => {
         setTheme(prevTheme => (prevTheme === "light" ? "dark" : "light"));
     };
 
     useEffect(() => {
-        localStorage.setItem("theme", theme); // Store theme preference in localStorage
+        // Store the theme preference in localStorage
+        localStorage.setItem("theme", theme);
     }, [theme]);
 
     return (
